bb91f3032e22999ae61dc59d7bc1205f0aabe7e8
huntum <mohit.jagtiani@sytac.io>
2020-05-05 21:55:59 +0200

    intial project add

---
+++ src/test/java/com/example/sfgpetclinicmj/SfgPetClinicMjApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.sfgpetclinicmj;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SfgPetClinicMjApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
