84867438a381b6b997dafc8229fae041adbbf99e
huntum <mohit.jagtiani@sytac.io>
2020-05-08 18:12:06 +0200

    added service maps, index page and controllers fro each entiry

---
+++ pet-clinic-mj-data/src/main/java/com/example/sfgpetclinicmj/service/map/VetMapService.java
@@ -0,0 +1,33 @@
+package com.example.sfgpetclinicmj.service.map;
+
+import com.example.sfgpetclinicmj.model.Vet;
+import com.example.sfgpetclinicmj.service.CrudService;
+import java.util.Set;
+
+public class VetMapService extends AbstractMapService<Vet, Long> implements CrudService<Vet, Long> {
+
+    @Override
+    public Set<Vet> findAll() {
+        return super.findAll();
+    }
+
+    @Override
+    public Vet findById(Long id) {
+        return super.findById(id);
+    }
+
+    @Override
+    public Vet save(Vet object) {
+        return super.save(object.getId(), object);
+    }
+
+    @Override
+    public void delete(Vet object) {
+        super.delete(object);
+    }
+
+    @Override
+    public void deleteById(Long id) {
+        super.deleteById(id);
+    }
+}
